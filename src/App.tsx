ab0ea675280888ff954edae4ca4dc3d1d4f5f204
KRGlobalLTD <krgloballtd@protonmail.com>
2025-08-10 22:45:45 +0700

    feat: add global whatsapp button

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,6 @@ import { HeroSection } from './components/HeroSection';
 import { AboutSection } from './components/AboutSection';
 import { Footer } from './components/Footer';
 import WhatsAppButton from '@/components/WhatsAppButton';
-import FabPortal from "@/components/FabPortal";
 import { ENABLE_DZ_PARTICLES } from './featureFlags';
 
 const DarkZoneParticles = React.lazy(() => import('./components/DarkZoneParticles'));
@@ -44,9 +43,7 @@ function App() {
       </main>
 
       <Footer />
-      <FabPortal>
-        <WhatsAppButton />
-      </FabPortal>
+      <WhatsAppButton />
       {ENABLE_DZ_PARTICLES && showParticles && (
         <React.Suspense fallback={null}>
           <DarkZoneParticles />
